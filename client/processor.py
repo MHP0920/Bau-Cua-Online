4df00b8d12a82c1713fa0acf6d312d002cdd4c85
unknown <minhhieuboy2009@gmail.com>
2023-01-28 02:25:11 +0700

    Luan An Tien Si CNTT <(,,) (Fix processor)

--- client/processor.py
+++ client/processor.py
@@ -1,7 +1,7 @@
 def _solve(_player: list, _server: list, _balance: int):
     for choice in set(_player):
         if choice in _server:
-            _balance += 5 * (_server.count(choice) + _player.count(choice))
+            _balance += 5 * _player.count(choice) * _server.count(choice)
         elif choice not in _server:
             _balance -= 5 * _player.count(choice)
     return _balance
